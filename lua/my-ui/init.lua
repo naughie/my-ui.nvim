174c064407d7f8a7622a17ce1b8e1e7bf1c69f6d
naughie <masaton@naughie.com>
2025-08-19 21:16:33 +0900

    Update background style

--- lua/my-ui/init.lua
+++ lua/my-ui/init.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 local bg_pat = {
-    hor = "⑅୨୧⑅",
-    ver = { "│", "*" },
+    hor = "｡o♡｡o｡",
+    ver = { left = { "୨୧", ":." }, right = { "୨୧", ".:" } },
     corner = "🎀",
 }
 M.bg_pat = bg_pat
@@ -30,7 +30,7 @@ local default_opts = {
                 return math.floor((api.nvim_get_option("columns") - dim.companion.width) / 2)
             end,
             row = function(dim)
-                return math.floor((api.nvim_get_option("lines") - dim.main.height) / 2) + dim.main.height + 1
+                return math.floor((api.nvim_get_option("lines") - dim.main.height) / 2) + dim.main.height + 3
             end,
         },
     },
@@ -44,6 +44,69 @@ local default_opts = {
     },
 }
 
+local function build_bg_pat(geom)
+    local base_width = geom.width + 2
+    local base_height = geom.height + 2
+
+    local pat_hor_len = vim.fn.strwidth(bg_pat.hor)
+    local copies = math.floor(base_width / pat_hor_len) + 1
+    local pat_width = pat_hor_len * copies
+
+    local diff = pat_width - base_width
+    local col_offset = math.floor(diff / 2)
+
+    local pat_cor_len = vim.fn.strwidth(bg_pat.corner)
+
+    local hor = bg_pat.corner .. string.rep(bg_pat.hor, copies) .. bg_pat.corner
+
+    local max_ver_width = { left = 0, right = 0 }
+    for _, pat in ipairs(bg_pat.ver.left) do
+        local max_width = math.max(string.len(pat), max_ver_width.left)
+        max_ver_width.left = max_width
+    end
+    for _, pat in ipairs(bg_pat.ver.right) do
+        local max_width = math.max(string.len(pat), max_ver_width.right)
+        max_ver_width.right = max_width
+    end
+
+    local lines = {}
+    local hl = {}
+
+    table.insert(lines, hor)
+    table.insert(hl, { line = 0, from = 0, to = -1 })
+
+    for i = 1, base_height do
+        local l_idx = i % #bg_pat.ver.left
+        if l_idx == 0 then l_idx = #bg_pat.ver.left end
+
+        local r_idx = i % #bg_pat.ver.right
+        if r_idx == 0 then r_idx = #bg_pat.ver.right end
+
+        local l_pat = bg_pat.ver.left[l_idx]
+        local r_pat = bg_pat.ver.right[r_idx]
+
+        local offset = 2 * pat_cor_len - vim.fn.strwidth(l_pat) - vim.fn.strwidth(r_pat)
+
+        table.insert(lines, l_pat .. string.rep(" ", pat_width + offset) .. r_pat)
+
+        local total_width = string.len(l_pat) + string.len(r_pat) + pat_width + offset
+        table.insert(hl, { line = i, from = 0, to = max_ver_width.left })
+        table.insert(hl, { line = i, from = total_width - max_ver_width.right, to = total_width })
+    end
+    table.insert(lines, hor)
+    table.insert(hl, { line = base_height + 1, from = 0, to = -1 })
+
+    return {
+        lines = lines,
+        width = pat_width + pat_cor_len * 2,
+        height = base_height + 2,
+        col = geom.col - pat_cor_len - col_offset - 1,
+        row = geom.row - 2,
+        hl = hl,
+    }
+end
+M.bg_pat.build = build_bg_pat
+
 local last_active_win = mkstate.tab()
 local ui_stack = mkstate.tab()
 
@@ -51,6 +114,7 @@ local ui_win_table = {}
 local all_ui = {}
 
 local augroup = api.nvim_create_augroup("NaughieMyui", { clear = true })
+local hl_ns = api.nvim_create_namespace("naughie_myui_bg_hl")
 
 -- path should be `vim.fn.fnameescape`d before call
 function M.open_file_into_current_win(path)
@@ -205,41 +269,21 @@ local function open_bg_with(geom, bg_states)
     local buf = bg_states.buf_id.get()
     if not buf then return end
 
-    local pat_hor_len = vim.fn.strwidth(bg_pat.hor)
-    local copies = math.floor(geom.width / pat_hor_len) + 1
-    local pat_width = pat_hor_len * copies
+    local bg = build_bg_pat(geom)
 
-    local diff = pat_width - geom.width
-    local col_offset = math.floor(diff / 2)
-
-    local pat_cor_len = vim.fn.strwidth(bg_pat.corner)
-
-    local hor = bg_pat.corner .. string.rep(bg_pat.hor, copies) .. bg_pat.corner
-    local ver = {}
-    for _, ver_pat in ipairs(bg_pat.ver) do
-        local offset = pat_cor_len - vim.fn.strwidth(ver_pat)
-        table.insert(ver, ver_pat .. string.rep(" ", pat_width + offset * 2) .. ver_pat)
-    end
-
-    local lines = {}
-    table.insert(lines, hor)
-    for i = 1, geom.height do
-        local idx = i % #ver
-        if idx == 0 then idx = #ver end
-        table.insert(lines, ver[idx])
-    end
-    table.insert(lines, hor)
-
-    api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+    api.nvim_buf_set_lines(buf, 0, -1, false, bg.lines)
 
     local bg_geom = {
-        width = pat_width + pat_cor_len * 2,
-        height = geom.height + 2,
-        col = geom.col - pat_cor_len - col_offset,
-        row = geom.row - 1,
+        width = bg.width,
+        height = bg.height,
+        col = bg.col,
+        row = bg.row,
     }
 
     open_float_with(buf, bg_geom, bg_states.win_id, true)
+    for _, hl in ipairs(bg.hl) do
+        api.nvim_buf_add_highlight(buf, hl_ns, "FloatBorder", hl.line, hl.from, hl.to)
+    end
 end
 
 local function declare_ui_one()
